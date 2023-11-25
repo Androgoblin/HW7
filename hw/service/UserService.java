8f70f97dbc2fcdbb01c9362b78a6c6b6a0016883
Radik <radikrah78@gmail.com>
2023-11-25 09:15:56 +0300

    hw6

---
+++ hw/service/UserService.java
@@ -0,0 +1,10 @@
+package service;
+
+import data.User;
+import java.util.List;
+
+public interface UserService <T extends User> {
+    void create (String firstName, String secondName, String lastName);
+    List<T> getAll();
+    
+}
---
+++ hw/view/UserView.java
@@ -0,0 +1,10 @@
+package view;
+
+import data.User;
+
+import java.util.List;
+
+public interface UserView  <T extends User>{
+    void sendOnConsole(List<T> list);
+    
+}
